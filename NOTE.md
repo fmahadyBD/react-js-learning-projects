b804ccde12483d65df37a81ce617fd6a43912460
Mahady Hasan Fahim <fmahady01@gmail.com>
2024-09-06 12:42:08 +0600

    create first commponent

---
+++ NOTE.md
@@ -0,0 +1,1 @@
+-tsx: this is for commponeds
---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+//paseCase
+function Message(){
+    //this is jsx: JavaScript XML
+
+    const name ='';
+    if(name)
+        return <h1>Hello {name}</h1>;
+    else
+        return <h1>Hello World</h1>
+}
+export default Message;
